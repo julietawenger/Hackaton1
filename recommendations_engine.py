49908dba40c174615653c461e447533339603ab4
Julieta Wenger <wenerjulieta@gmail.com>
2025-03-02 13:44:31 +0200

    recommendations engine modifications

--- recommendations_engine.py
+++ recommendations_engine.py
@@ -1,22 +1,22 @@
-#%%
+
 from simulate_users import book_df 
-from simulate_users import exploded_df
-from simulate_users import user_data
 from scipy.stats import pearsonr
 import numpy as np
 import pandas as pd
 import ast
 
 user_df = pd.read_csv(r'users.csv')
-#%%
 
-#%%
-def recommend_by_total_rating(user_df, book_df, target_user_ID):
-    
+def explode_df(book_df):
+    exploded_df = book_df.explode('genre')
+    exploded_df = exploded_df.reset_index(drop=True)
+    return exploded_df
+
+def recommend_by_total_rating(book_df, user_df, target_user_ID):
+    book_df = explode_df(book_df)
     # First I need to access the user preferences
-    user_preferences = user_df.iloc[target_user_ID]["preferences"]
+    user_preferences = ast.literal_eval(user_df.iloc[target_user_ID]["preferences"])
     # I need to filter the dataframe based on the user's preferences 
-
     filtered_books = book_df[book_df['genre'].apply(lambda x: any(g in x for g in user_preferences))]
     # Sort by rating
     filtered_books = filtered_books.sort_values(by = 'rating', ascending=False)
@@ -32,7 +32,6 @@ def recommend_by_total_rating(user_df, book_df, target_user_ID):
         authors.append(list(book_df[book_df["title"]== i]['author'])[0])
     return titles, authors      
     
-#%%
 
 def book_hist_to_df(user_df, target_user_ID):
     """This function takes a row from the users database and returns the users reading history in a DataFrame."""    
@@ -49,18 +48,19 @@ def user_book_rating_df(user_df):
     ratings_df = pd.concat(ratings_df_list, ignore_index= True) 
     return ratings_df
 
-#If a user rated the same book multiple times, take the average rating.
-ratings_df = user_book_rating_df(user_df).groupby(["user_id", "book"], as_index=False).agg({"rating": "mean"})
-#%%
+def ratings_matrix(user_df):
+    #If a user rated the same book multiple times, take the average rating.
+    ratings_df = user_book_rating_df(user_df).groupby(["user_id", "book"], as_index=False).agg({"rating": "mean"})
 
-# Rating matrix has the size books x users and if they read the book, the value is the rating. Otherwise it's 0.
-rating_matrix = ratings_df.pivot(index="user_id", columns="book", values="rating")
-rating_matrix = rating_matrix.fillna(0)  # Fill missing values with 0
-
-#%%
-
-def find_similar_users(user_id, rating_matrix):
+    # Rating matrix has the size books x users and if they read the book, the value is the rating. Otherwise it's 0.
+    rating_matrix = ratings_df.pivot(index="user_id", columns="book", values="rating")
+    rating_matrix = rating_matrix.fillna(0)  # Fill missing values with 0
+    return rating_matrix
+#
+def find_similar_users(user_id, user_df):
     """This function finds users who correlate to the target user."""
+    
+    rating_matrix = ratings_matrix(user_df)
     similarities = {}
     target_user_ratings = rating_matrix.loc[user_id]
     
@@ -71,8 +71,11 @@ def find_similar_users(user_id, rating_matrix):
 
     return sorted(similarities.items(), key=lambda x: x[1], reverse=True) 
 
-def recommend_books_by_users(user_id, rating_matrix, top_n=5):
-    similar_users = find_similar_users(user_id, rating_matrix)[:5]
+def recommend_books_by_users(book_df, user_df, user_id):
+    book_df = explode_df(book_df)
+    rating_matrix = ratings_matrix(user_df)
+
+    similar_users = find_similar_users(user_id, user_df)[:5]
     similar_users_ids = [user[0] for user in similar_users]
     
     # Get books rated highly by similar users
@@ -81,29 +84,26 @@ def recommend_books_by_users(user_id, rating_matrix, top_n=5):
 
     # Exclude books already rated by the target user
     user_rated_books = set(rating_matrix.loc[user_id][rating_matrix.loc[user_id] > 0].index)
-    recommendations = avg_ratings.drop(user_rated_books).head(top_n)
+    recommendations = avg_ratings.drop(user_rated_books).head()
 
     # Keep only recommendations with correlations higher than zero.
     recommendations = recommendations[recommendations > 0] 
+    if len(recommendations) >0:
+        books=[]
+        authors=[]
+        for i in recommendations.items():
+            book = i[0]
+            author = book_df.loc[book_df['title'] == book, 'author'].iloc[0]
+            books.append(book)
+            authors.append(author)
 
-    return [i[0] for i in recommendations.items()],avg_ratings
-#%%
-#%%
-user_id = 20
-top_n = 5
-similar_users = find_similar_users(user_id, rating_matrix)[:5]
-similar_users_ids = [user[0] for user in similar_users]
-
-# Get books rated highly by similar users
-similar_users_ratings = rating_matrix.loc[similar_users_ids]
-avg_ratings = similar_users_ratings.mean().sort_values(ascending=False)
-print([corr for book, corr in avg_ratings])
-    # Arreglar que si no tiene correlacion que no imprima
-#%%
-user_df.iloc[20]
+        return books, authors
 
 
-recommended_books = recommend_books_by_users(20, rating_matrix)
-recommended_books_2 = recommend_by_total_rating(user_df, exploded_df, 20)
-print(user_df.iloc[20])
-print(recommended_books, recommended_books_2)
+
+
+print(user_df.iloc[3]["preferences"])
+recommended_books = recommend_books_by_users(book_df, user_df, 3)
+recommended_books_2 = recommend_by_total_rating(book_df, user_df,3)
+print(recommended_books)
+print(recommended_books_2)
